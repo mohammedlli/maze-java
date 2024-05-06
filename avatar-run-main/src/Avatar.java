7a66f747e99128b144521f1a2b9eb8f7cdb66ac8
Mohammed Ahmed <129491219+mohammedlli@users.noreply.github.com>
2024-05-06 18:37:09 +0300

    Update Avatar.java

--- avatar-run-main/src/Avatar.java
+++ avatar-run-main/src/Avatar.java
@@ -9,11 +9,6 @@ import java.awt.event.KeyEvent;
 import java.util.Base64;
 
 import javax.crypto.SecretKey;
-/**
- * AVATAR (CREACION DE PERSONAJE Y DEFINICION DE MOVIMIENTOS)
- * @author fucalex
- * 
- */
 public class Avatar {
     Maze lab=new Maze();
     public  static int x=40;
@@ -36,96 +31,54 @@ public class Avatar {
         }
         return null;
     }
-    /**
-    *FORMA Y COLOR DEL PERSONAJE
-    * Se definen las caracteristicas tales como forma, tamaño y colores
-    * del avatar.Java proporciona la clase Graphics, que permite dibujar
-    * elipses, cuadrados, líneas, mostrar texto y también tiene muchos 
-    * otros métodos de dibujo. Para poder pintar, debemos crear un componente 
-    * y pasarlo al programa como un argumento al método paint().
-     * @param grafico
-    */
-        /**
-         * Base (Este es el "espacio personal" del avatar. 
-         * Se toma los valores de ubicacion y tamaño definidos en clase publica. 
-         */
     public void paint(Graphics grafico){
         grafico.setColor(Color.red);
         grafico.fillRect(x, y, width, height);
         grafico.setColor(Color.BLACK);
         grafico.drawRect(x, y, width, height);
-        
-        /**
-         *Cabeza (Para todos los siguiente elementos, se hacen los graficos, 
-         *se ubican en coordenadas y se les da color)
-         */
         grafico.setColor(Color.black);
         grafico.fillOval(x+16,y+3,8,8);
         grafico.setColor(Color.black);
-        
-        //Brazos y hombros
         grafico.fillRect(x+9,y+12,4,14);
         grafico.fillRect(x+11,y+12,4,4);
         grafico.fillRect(x+28,y+12,4,14);
         grafico.fillRect(x+26,y+12,4,4);
          grafico.fillRect(x+14,y+12,13,14);
-       
-        //Piernas
         grafico.fillRect(x+14,y+24,6,14);
         grafico.fillRect(x+21,y+24,6,14);
     }
-    
-    /**
-    *CAPTURA DE MOVIMIENTO
-    * Para indicar cuales son las teclas que funcionan en el juego, se indica
-    * el codigo de las teclas de direccion. 
-    * Aqui tambien se indica por qué el avatar se debe mover a ciertos lados.
-    * "!=" indica que puede moverse a cualquier elemento que no sea 1 (mas adelante
-    * se explica que 1, corresponde a un bloque del laberinto).
-    * @param event
-    * @see https://www.yoelprogramador.com/teclas-del-teclado-y-valores-de-codigos-de-tecla/
-    */
-    
+
     public void teclaPresionada(KeyEvent event) throws Exception {
 
 
 
         int[][]maze=lab.getMaze();
         
-        if(event.getKeyCode()==37){//Mover a la izquierda
+        if(event.getKeyCode()==37){
             if(maze[y/40][(x/40)-1] !=1){
                 x=x-movement;
 
             }
         }
-        if(event.getKeyCode()==39){//Mover a la derecha
+        if(event.getKeyCode()==39){
             if(maze[y/40][(x/40)+1]!=1){
                 x=x+movement;
 
             }
         }
-        if(event.getKeyCode()==40){//Mover abajo
+        if(event.getKeyCode()==40){
             if(maze[(y/40)+1][x/40]!=1){
                 y=y+movement;
 
             }
         }
-        if(event.getKeyCode()==38){//Mover arriba
+        if(event.getKeyCode()==38){
             if(maze[(y/40)-1][x/40]!=1){
                 y=y-movement;
 
 
             }
         }
-
-        /**
-        *SIGUIENTE NIVEL
-        * Para alcanzar el final del nivel, el jugador debe estar ubicado en la posicion de x, y, de la esquina
-        * inferior derecha, donde se activará el cambio de nivel y el avatar será ubicado en la esquina
-        * superior izquierda del siguiente nivel
-        */
-
-
         if(x==840 && y==440){
 
             String input = JOptionPane.showInputDialog("<html><body style='width: 100%; padding: 3px; border-style: dashed; borderRadius: 10px; text-align: center; background:rgb(120 53 15); border-radius: 5px; border:6px solid rgb(69 26 3); '>"
